a91aaf1bb73ffe1e22fee6e6ee7f2dcfc3f04360
clint21.eth ⚡️ <me@clint21.dev>
2025-06-01 21:59:38 +0200

    Fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function Page () {
 	return ( 
-		<Flex justifyContent={"center"} alignItems={"center"} className={"min-h-full text-center px-6 py-24 sm:py-32 lg:px-8"}>
+		<Flex justifyContent={"center"} flexDirection={"col"} alignItems={"center"} className={"min-h-full text-center px-6 py-24 sm:py-32 lg:px-8"}>
 			<p className={"text-base font-semibold"}>Error 404</p>
 			<h1 className={"mt-4 text-3xl font-bold tracking-tight sm:text-5xl"}>Page Not Found</h1>
 			<p className={"mt-6 text-base leading-7 text-gray-600"}>Sorry, we couldn’t find the page you’re looking for.</p>
